7682c45efe1b21024b58a2f5191b4e1be236af86
Anthony Navarre <anthony.navarre@factorylabs.com>
2011-03-14 21:18:37 -0600

    refactor HTTParty default params

--- lib/tangerine/backlot.rb
+++ lib/tangerine/backlot.rb
@@ -13,23 +13,14 @@ module Tangerine
     class API
 
       def self.authenticate!(credentials)
-        @provider_code = credentials[:provider_code]
         @secret = credentials[:secret]
+        HTTP.default_params :pcode => credentials[:provider_code]
       end
 
       def self.get(request_type, params={})
-        set_params(params)
-        HTTP.get(request_type)
-      end
-
-      def self.set_params(params)
-        HTTP.default_params.clear
-
         params['expires'] ||= (Time.now.to_i + 10).to_s
-        HTTP.default_params params
-
-        HTTP.default_params :pcode => @provider_code
-        HTTP.default_params :signature => self.signature(params)
+        params['signature'] = self.signature(params)
+        HTTP.get(request_type, {:query => params})
       end
 
       def self.signature(params)
